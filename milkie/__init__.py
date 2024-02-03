46caa5de1b3a803d1ffa0c836949474077a60ac8
xforce-io <xupeng@192.168.1.3>
2024-02-03 22:54:30 +0800

    develop

--- milkie/__init__.py
+++ milkie/__init__.py
@@ -1,4 +1,3 @@
 import logging, sys
 
 logging.basicConfig(filename='log/example.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
