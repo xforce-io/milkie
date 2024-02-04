d0766718db78da70be17e875f75afaebb2f517bd
xforce-io <xupeng@xupengdeMacBook-Pro.local>
2024-02-04 09:53:21 +0800

    develop

--- milkie/retrieval/retrievers.py
+++ milkie/retrieval/retrievers.py
@@ -1,8 +1,11 @@
+import logging
 from typing import List
 from llama_index import VectorStoreIndex
 from llama_index.indices.query.schema import QueryBundle
 from llama_index.retrievers import BaseRetriever, BM25Retriever
 from llama_index.schema import NodeWithScore
+
+logger = logging.getLogger(__name__)
 
 class HybridRetriever(BaseRetriever):
     def __init__(
@@ -18,6 +21,7 @@ class HybridRetriever(BaseRetriever):
         nodeIdToNode = {}
         if self.denseRetriever:
             vectorNodes = self.denseRetriever._retrieve(query_bundle)
+            logger.debug(f"dense_retriever_recall_num[{len(vectorNodes)}]")
             for vectorNode in vectorNodes:
                 if vectorNode.score < 0.4:
                     continue
@@ -32,6 +36,7 @@ class HybridRetriever(BaseRetriever):
 
         if self.sparseRetriever:
             bm25Nodes = self.sparseRetriever._retrieve(query_bundle)
+            logger.debug(f"dense_retriever_recall_num[{len(bm25Nodes)}]")
             for bm25Node in bm25Nodes:
                 theNode = nodeIdToNode.get(bm25Node.node_id)
                 if theNode is None:
