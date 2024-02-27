c52d5038d874109384f10dffce756b9cc4d17dd3
xforce-io <xupeng@xupengdeMacBook-Pro.local>
2024-02-27 13:53:18 +0800

    develop

--- milkie/model_factory.py
+++ milkie/model_factory.py
@@ -1,14 +1,33 @@
+from typing import Sequence
 import torch
 import logging, json
 from dataclasses import asdict
 from llama_index import PromptTemplate
 from llama_index.llms import HuggingFaceLLM
 from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index.llms.types import ChatMessage
 
 from milkie.prompt.prompt import Loader
 from milkie.config.config import EmbeddingConfig, LLMConfig
 
+from llama_index.llms.generic_utils import (
+    messages_to_prompt as generic_messages_to_prompt,
+)
+
 SystemPromptCn = Loader.load("system_prompt")
+
+def tokenizerMessagesToPrompt(self, messages: Sequence[ChatMessage]) -> str:
+    if hasattr(self._tokenizer, "apply_chat_template"):
+        messages_dict = [
+            {"role": message.role.value, "content": message.content}
+            for message in messages
+        ]
+        tokens = self._tokenizer.apply_chat_template(
+            messages_dict, 
+            generic_messages_to_prompt=True)
+        return self._tokenizer.decode(tokens)
+
+    return generic_messages_to_prompt(messages)
 
 class ModelFactory:
     
@@ -26,6 +45,7 @@ class ModelFactory:
                 query_wrapper_prompt=PromptTemplate("{query_str}\n<|ASSISTANT|>\n"),
                 tokenizer_name=config.model,
                 model_name=config.model,
+                messages_to_prompt=tokenizerMessagesToPrompt,
                 device_map="auto",
                 stopping_ids=[50278, 50279, 50277, 1, 0],
                 tokenizer_kwargs={"max_length": config.ctxLen, "use_fast": False, "trust_remote_code": True},
