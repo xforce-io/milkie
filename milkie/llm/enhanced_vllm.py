1cd61bb96b10811f6c294e6f162c30e6a30d99d6
xforce-io <xupeng@xupengdeMacBook-Pro.local>
2024-05-09 21:17:05 +0800

    develop

--- milkie/llm/enhanced_vllm.py
+++ milkie/llm/enhanced_vllm.py
@@ -1,133 +1,18 @@
-from typing import Any, Dict, Optional, Sequence
-from queue import Queue
+from typing import Any
 import torch
 from vllm import SamplingParams
 from vllm.engine.arg_utils import EngineArgs
 from vllm.engine.llm_engine import LLMEngine
 
-from llama_index.legacy.llms.generic_utils import (
-    completion_response_to_chat_response,
-)
 from llama_index.core.prompts.base import BasePromptTemplate
-from llama_index.legacy.llms.custom import CustomLLM
 from llama_index_client import BasePromptTemplate
 from llama_index.legacy.core.llms.types import (
-    ChatMessage,
-    ChatResponse,
-    ChatResponseGen,
-    ChatResponseAsyncGen,
     CompletionResponse, 
-    CompletionResponseGen,
-    CompletionResponseAsyncGen,
-    LLMMetadata,
 )
 from llama_index.legacy.llms.vllm import Vllm
-from llama_index.legacy.llms.base import llm_chat_callback, llm_completion_callback
-from llama_index.legacy.bridge.pydantic import Field, PrivateAttr
 
 from milkie.config.config import QuantMethod
-from milkie.llm.enhanced_llm import EnhancedLLM, QueueRequest, QueueResponse
-
-class VLLM(CustomLLM):
-
-    model: Optional[str] = Field(description="The Vllm Model to use.")
-
-    max_new_tokens: int = Field(
-        default=512,
-        description="Maximum number of tokens to generate per output sequence.",
-    )
-
-    dtype: str = Field(
-        default="auto",
-        description="The data type for the model weights and activations.",
-    )
-    
-    _engine: Any = PrivateAttr()
-    
-    def __init__(
-            self, 
-            model_name: str,
-            context_window: int,
-            max_new_tokens :int,
-            dtype :str,
-            vllm_kwargs :Dict[str, Any]) -> None:
-        super().__init__(model=model_name, max_new_tokens=max_new_tokens)
-
-        engineArgs = EngineArgs(
-            model=model_name,
-            max_model_len=context_window,
-            dtype=dtype,
-            **vllm_kwargs)
-        self._engine = LLMEngine.from_engine_args(engineArgs)
-        
-    def class_name(cls) -> str:
-        return "VLLM"
-
-    @property
-    def engine(self) -> Any:
-        return self._engine
-
-    @property
-    def metadata(self) -> LLMMetadata:
-        return LLMMetadata(model_name=self.model)
-
-    @property
-    def _model_kwargs(self) -> Dict[str, Any]:
-        base_kwargs = {
-            "max_tokens": self.max_new_tokens,
-        }
-        return {**base_kwargs}
-
-    @llm_chat_callback()
-    def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
-        kwargs = kwargs if kwargs else {}
-        prompt = self.messages_to_prompt(messages)
-        completion_response = self.complete(prompt, **kwargs)
-        return completion_response_to_chat_response(completion_response)
-
-    @llm_completion_callback()
-    def complete(
-        self, prompt: str, formatted: bool = False, **kwargs: Any
-    ) -> CompletionResponse:
-        raise (ValueError("Not Implemented"))
-
-    @llm_chat_callback()
-    def stream_chat(
-        self, messages: Sequence[ChatMessage], **kwargs: Any
-    ) -> ChatResponseGen:
-        raise (ValueError("Not Implemented"))
-
-    @llm_completion_callback()
-    def stream_complete(
-        self, prompt: str, formatted: bool = False, **kwargs: Any
-    ) -> CompletionResponseGen:
-        raise (ValueError("Not Implemented"))
-
-    @llm_chat_callback()
-    async def achat(
-        self, messages: Sequence[ChatMessage], **kwargs: Any
-    ) -> ChatResponse:
-        kwargs = kwargs if kwargs else {}
-        return self.chat(messages, **kwargs)
-
-    @llm_completion_callback()
-    async def acomplete(
-        self, prompt: str, formatted: bool = False, **kwargs: Any
-    ) -> CompletionResponse:
-        raise (ValueError("Not Implemented"))
-
-    @llm_chat_callback()
-    async def astream_chat(
-        self, messages: Sequence[ChatMessage], **kwargs: Any
-    ) -> ChatResponseAsyncGen:
-        raise (ValueError("Not Implemented"))
-
-    @llm_completion_callback()
-    async def astream_complete(
-        self, prompt: str, formatted: bool = False, **kwargs: Any
-    ) -> CompletionResponseAsyncGen:
-        raise (ValueError("Not Implemented"))
-
+from milkie.llm.enhanced_llm import EnhancedLLM
 
 class EnhancedVLLM(EnhancedLLM):
     def __init__(self,
@@ -141,7 +26,6 @@ class EnhancedVLLM(EnhancedLLM):
         super().__init__(context_window, concurrency, tokenizer_name, device, tokenizer_kwargs)
         
         self._llm = Vllm(
-            context_window=context_window,
             model_name=model_name,
             max_new_tokens=max_new_tokens,
             dtype="auto",
@@ -181,12 +65,6 @@ class EnhancedVLLM(EnhancedLLM):
             prompts: list[str], 
             **kwargs: Any
     ) -> list[CompletionResponse]:
-        #return self._completeBatchAsync(
-        #    prompts=prompts, 
-        #    numThreads=1,
-        #    inference=EnhancedVLLM._inference,
-        #    tokenIdExtractor=lambda output : output.outputs[0].token_ids,
-        #    **kwargs)
         return self._completeBatchSync(prompts, **kwargs)
 
     def _completeBatchSync(
@@ -216,44 +94,6 @@ class EnhancedVLLM(EnhancedLLM):
                 text=output.outputs[0].text,
                 raw={"model_output": output.outputs[0].token_ids},)]
         return result
-
-    def _inference(
-            self, 
-            reqQueue :Queue[QueueRequest], 
-            resQueue :Queue[QueueResponse], 
-            genArgs :dict,
-            **kwargs :Any) -> Any:
-        genArgs = genArgs if genArgs else {}
-        params = {
-            **self._llm._model_kwargs, 
-            **EnhancedLLM.filterGenArgs(genArgs)}
-        samplingParams = SamplingParams(**params)
-        while not reqQueue.empty():
-            for i in range(self.concurrency):
-                if reqQueue.empty():
-                    break
-
-                request = reqQueue.get()
-                if not request:
-                    break
-                
-                self._llm.engine.add_request(
-                    prompt=request.prompt,
-                    prompt_token_ids=request.tokenized, 
-                    sampling_params=samplingParams, 
-                    request_id=request.requestId)
-
-            outputs = []
-            while self._llm.engine.has_unfinished_requests():
-                stepOutputs = self._llm.engine.step()
-                for requestOutput in stepOutputs:
-                    if requestOutput.finished:
-                        outputs.append(requestOutput)
-
-            for output in outputs:
-                resQueue.put(QueueResponse(
-                    requestId=output.request_id, 
-                    output=output))
 
     def _getSingleParameterSizeInBytes(self):
         type_to_size = {
