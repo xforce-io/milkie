e0eeb9bfdd599455a64a418fa77565840e984844
xforce-io <superxup@gmail.com>
2024-08-13 20:37:38 +0800

    develop

--- data_process/file_metadata.py
+++ data_process/file_metadata.py
@@ -3,14 +3,23 @@ import json
 import datetime
 from pathlib import Path
 
+from milkie.agent.team.mrqa import MapReduceQA
+
 SupportedFormat = ["pdf"]
 
-def get_file_metadata(file_path):
+def get_file_metadata(file_path, summaryAgent):
     file_stat = os.stat(file_path)
+
     metadata = {
         "name": os.path.basename(file_path),
         "size": file_stat.st_size,
     }
+
+    response = summaryAgent.execute(
+        input="@filepath:%s" % file_path, 
+        task="写一段概要总结，不超过 200 字")
+    if response is not None:
+        metadata["summary"] = response
 
     #response = chat(
     #    llm=self.roleAgent.context.globalContext.settings.llm, 
@@ -24,10 +33,11 @@ def get_file_metadata(file_path):
     return None
 
 def get_all_files_metadata(directory):
+    summaryAgent = MapReduceQA()
     for root, _, files in os.walk(directory):
         for name in files:
             file_path = os.path.join(root, name)
-            yield get_file_metadata(file_path)
+            yield get_file_metadata(file_path, summaryAgent)
 
 def main():
     directory = "/Users/xupeng/Documents/eb/materials"
