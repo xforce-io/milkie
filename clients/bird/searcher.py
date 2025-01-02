ce1dbce7fe0fe786fc7c6d90e5eff0454a38b8d1
xforce-io <superxup@gmail.com>
2025-01-02 13:28:07 +0800

    develop

--- clients/bird/searcher.py
+++ clients/bird/searcher.py
@@ -28,7 +28,7 @@ class Searcher:
             
         return escape(f"""
     [{self._get_thought_model()}] (trial: {trial}) 请根据请求中包括的 schema、问题做分析，一步一步思考，给出问题的解决思路
-    schema及问题 ```{query}```
+    schema及问题 ```{query[query.index("--")+2:]}```
     可用schema解释 ```{self._db.descTablesFromQuery(query)}```
     已有错误模式 ```{error_hints}```
 
@@ -189,6 +189,9 @@ class Searcher:
         except Exception as e:
             ERROR(f"Error in inference: {str(e)}")
             raise
+
+    def _extract_query_for_prompt(self, query: str) -> str:
+        return query[query.index("--")+2:]
 
     def _get_thought_model(self) -> str:
         return self.config.model.thought_model
